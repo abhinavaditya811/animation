c471c201cacc7c0d5271bc37f3d1f73cef0ad655
Abhinav Aditya <40894851+abhinavaditya811@users.noreply.github.com>
2022-05-04 11:17:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [arr, setArr] = useState([1, 2]);
+	const incrementState = () => {
+		if(arr.length < 100)
+		setArr([...arr, arr.at(-1) + 1]);
+	};
+
+	const returnNumber = (num) => {
+		if (num % 6 === 0 && arr.length === 100) {
+			return <div className="number orange">{num}</div>;
+		} else if (num % 3 === 0 && arr.length === 100) {
+			return <div className="number green">{num}</div>;
+		} else if (num % 2 === 0 && arr.length === 100){
+			return <div className="number blue">{num}</div>;
+		} else {
+			return <div className="number">{num}</div>;
+		}
+	}
+
+	useEffect(() => {
+		setTimeout(incrementState, 1000);
+	}, [arr]);
+
+	const colouredVals = (
+		<div className="display-num">
+			{arr.map((num) => {
+				console.log(arr)
+				return returnNumber(num)
+			})}
+		</div>
+	);
+
+	return <>{colouredVals}</>;
 }
 
 export default App;
